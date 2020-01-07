e14aecd3bd4cc64874ab048cec7457faec308101
JoeSong168 <joesong168@qq.com>
2020-01-07 10:44:11 +0800

    focus version

--- single-dl.sh
+++
@@ -1,8 +0,0 @@
-
-youtube-dl -o '%(playlist)s-%(playlist_index)s-%(title)s.%(ext)s' $1
-qshell account G8vPvJGMMZ_2mCLrTpHUDuuHWHZYg0JoUVpFEPHR eK6IFx3sqEg7yTRmL7VsNyx3JGSybesJlso1gy96 default
-for file in *
-do
-    qshell rput gsh-usa "$file" "$file"
-done
-rm *
